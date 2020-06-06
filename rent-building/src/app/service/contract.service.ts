927cb45a8c6980027054cddf4dec988d539da073
Admin <dinhhoa1994@gmail.com>
2020-06-06 10:43:38 +0700

    sd

--- rent-building/src/app/service/contract.service.ts
+++ rent-building/src/app/service/contract.service.ts
@@ -22,8 +22,8 @@ export class ContractService extends CrudService<ContractModel, number> {
     const color = Math.floor((Math.random() * 4) + 1);
     $.notify({
       icon: 'notifications',
-      title1: title,
-      message1: message,
+      title: title,
+      message: message,
     }, {
       type: type[color],
       timer: 1500,
@@ -35,8 +35,8 @@ export class ContractService extends CrudService<ContractModel, number> {
         '<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">' +
         '<button mat-button  type="button" aria-hidden="true" class="close mat-button" data-notify="dismiss">  <i class="material-icons">close</i></button>' +
         '<i class="material-icons" data-notify="icon">notifications</i> ' +
-        '<span data-notify="title1">{1}</span> ' +
-        '<span data-notify="message1">{2}</span>' +
+        '<span data-notify="title">{1}</span> ' +
+        '<span data-notify="message">{2}</span>' +
         '<div class="progress" data-notify="progressbar">' +
         '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
         '</div>' +
