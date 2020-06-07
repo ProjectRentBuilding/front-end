3a5f099424ed37303cb6f34b7209fe29018b3b54
LeDinhQuoc24 <kudo2412@gmail.com>
2020-06-07 20:34:01 +0700

    fix

--- rent-building/src/app/component/ground/ground-list/ground-list.component.ts
+++ rent-building/src/app/component/ground/ground-list/ground-list.component.ts
@@ -68,8 +68,8 @@ export class GroundListComponent implements OnInit, OnDestroy {
   openDialogEdit(id): void {
     this.groundService.findOne(id).subscribe(dataOfGroundModel => {
       const dialogRef = this.dialog.open(GroundEditComponent, {
-        width: '1200px',
-        height: '840px',
+        width: '800',
+        height: '540px',
         data: {data1: dataOfGroundModel},
         disableClose: true,
       });
