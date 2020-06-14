510e32bdac7a03aebdf962f151f9f12697101b83
Minh Hùng Võ <nguyengiacat1@gmail.com>
2020-06-14 12:06:08 +0700

    hung update

--- rent-building/src/app/component/equipment/equipment-list/equipment-list.component.ts
+++ rent-building/src/app/component/equipment/equipment-list/equipment-list.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Subscription} from 'rxjs';
 import {EquipmentService} from '../../../service/equipment.service';
 import {MatDialog} from '@angular/material';
@@ -29,30 +29,47 @@ export class EquipmentListComponent implements OnInit, OnDestroy {
   public checkEdit = false;
   public checkAdd = false;
   public searchText;
-
+  public equipment: FormArray;
+  public getarray = 1;
   constructor(
     public formBuilder: FormBuilder,
     public equipmentService: EquipmentService,
     public groundService: GroundService,
-    public typeElementService : TypeEquipmentService,
+    public typeElementService: TypeEquipmentService,
     public dialog: MatDialog,
     public router: Router
   ) {
+    this.formAddNewEquipment = this.formBuilder.group({
+      equipment: this.formBuilder.array([this.createEquipment()])
+    });
   }
 
   ngOnInit() {
-    this.subscription = this.typeElementService.findAll().subscribe((data: TypeEquipmentModel[]) => {
+    this.typeElementService.findAll().subscribe((data: TypeEquipmentModel[]) => {
       this.typeEquipment = data;
     });
-    this.subscription = this.groundService.findAll().subscribe((data: GroundModel[]) => {
+    this.groundService.findAll().subscribe((data: GroundModel[]) => {
       this.grounds = data;
     });
-    this.subscription = this.equipmentService.findAll().subscribe((data: EquipmentModel[]) => {
+    this.equipmentService.findAll().subscribe((data: EquipmentModel[]) => {
       this.equipmentModel = data;
     });
-    this.formAddNewEquipment = this.formBuilder.group({
-      id:[''],
+    // this.formAddNewEquipment = this.formBuilder.group({
+    //   id:[''],
+    //   typeEquipmentId: ['', [Validators.required]],
+    //   nameEquipment: ['', [Validators.required]],
+    //   amount: ['', [Validators.required, Validators.pattern('^[0-9]{1,4}$')]],
+    //   amountOfBroken: ['', [Validators.required]],
+    //   note: ['', [Validators.required]],
+    //   groundId: ['', [Validators.required]],
+    // });
+  }
+
+  createEquipment(): FormGroup {
+    return this.formBuilder.group({
+      id: [''],
       typeEquipmentId: ['', [Validators.required]],
+      // typeEquipmentId: [''],
       nameEquipment: ['', [Validators.required]],
       amount: ['', [Validators.required, Validators.pattern('^[0-9]{1,4}$')]],
       amountOfBroken: ['', [Validators.required]],
@@ -67,20 +84,54 @@ export class EquipmentListComponent implements OnInit, OnDestroy {
     }
   }
 
-  checkaddNewEquipment() {
-    if (!this.checkAdd) {
-      // this.ngOnInit();
-      this.checkAdd = true;
-      this.checkEdit = false;
-    }
+  logValue() {
+    console.log(this.formAddNewEquipment.value);
+
   }
 
-  addNewEquipment() {
-    this.equipmentService.save(this.formAddNewEquipment.value).subscribe(data => {
-      this.checkAdd = false;
-      this.redirectTo('equipments');
-      this.equipmentService.showNotification('', 'Thêm mới thành công, chúc mừng bạn');
+  get equipmentControls() {
+    return this.formAddNewEquipment.get('equipment')['controls'];
+  }
+
+  addNewArray(): void {
+    this.checkAdd = true;
+    this.getarray ++;
+    this.subscription = this.typeElementService.findAll().subscribe((data: TypeEquipmentModel[]) => {
+      this.typeEquipment = data;
     });
+    this.equipment = this.formAddNewEquipment.get('equipment') as FormArray;
+    this.equipment.push(this.createEquipment());
+  }
+
+  removeAddress(i: number) {
+    this.equipment.removeAt(i);
+  }
+
+  // checkaddNewEquipment() {
+  //   if (!this.checkAdd) {
+  //     // this.ngOnInit();
+  //     this.checkAdd = true;
+  //     this.checkEdit = false;
+  //   }
+  // }
+  // saveEquipment(model: any, isValid: boolean, e: any){
+  //   e.preventDefault();
+  //   alert('Form data are: ' + model.at(1));
+  //
+  // }
+
+  addNewEquipment() {
+
+    this.equipment = this.formAddNewEquipment.get('equipment') as FormArray;
+    console.log((this.equipment.at(0).value));
+    for(let tem =0; tem < this.getarray; tem++){
+      // @ts-ignore
+      this.equipmentService.save(this.equipment.at(tem).value).subscribe(data => {
+        this.equipmentService.showNotification('', 'Thêm mới thành công, chúc mừng bạn');
+      });
+    }
+    // // this.checkAdd = false;
+    this.redirectTo('equipments');
     console.log(this.formAddNewEquipment);
   }
 
@@ -112,21 +163,23 @@ export class EquipmentListComponent implements OnInit, OnDestroy {
     this.redirectTo('equipments');
   }
 
-  openDialogDelete(id): void {
-    this.equipmentService.findOne(id).subscribe(dataOfEquipment => {
-      const dialogRef = this.dialog.open(EquipmentDeleteComponent, {
-        width: '500px',
-        data: {data1: dataOfEquipment},
-        disableClose: true,
-      });
-      dialogRef.afterClosed().subscribe(result => {
-        console.log('The dialog was closed');
-        this.ngOnInit();
-      });
-    });
-  }
-  searchType(text) {
-    console.log(text);
-    this.searchText = document.getElementById(text).innerText;
-  }
+  // openDialogDelete(id): void {
+  //   this.equipmentService.findOne(id).subscribe(dataOfEquipment => {
+  //     const dialogRef = this.dialog.open(EquipmentDeleteComponent, {
+  //       width: '500px',
+  //       data: {data1: dataOfEquipment},
+  //       disableClose: true,
+  //     });
+  //     dialogRef.afterClosed().subscribe(result => {
+  //       console.log('The dialog was closed');
+  //       this.ngOnInit();
+  //     });
+  //   });
+  // }
+  // searchType(text) {
+  //   console.log(text);
+  //   this.searchText = document.getElementById(text).innerText;
+  // }
+
+
 }
