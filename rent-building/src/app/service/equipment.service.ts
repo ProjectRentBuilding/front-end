799d3d15faa966b6fc1ba7b19cbcda48b6af5d9b
Minh Hùng Võ <nguyengiacat1@gmail.com>
2020-06-15 09:59:16 +0700

    hung up

--- rent-building/src/app/service/equipment.service.ts
+++ rent-building/src/app/service/equipment.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+
 
 import * as jQuery from 'jquery';
 import 'bootstrap-notify';
